36ab15ddf61171c172f96fbb255ca05f9f826410
sharon9457 <sharon.cho.tw@gmail.com>
2026-08-19 02:26:31 +0800

    Create OrderCompositionDemo.java

---
+++ 0818/OrderCompositionDemo.java
@@ -0,0 +1,36 @@
+class Customer{
+    private String id;
+    private String name;
+
+    Customer(String id , String name){
+        this.id=id;
+        this.name = name;
+    }
+    String label(){
+        return id +" "+name;
+    }
+}
+class Order{
+    private String orderId;
+    private Customer customer;
+    private int total;
+
+    Order(String orderId,Customer customer,int total){
+        this.orderId = orderId;
+        this.customer = customer;
+        this.total = Math.max(0,total);
+    }
+    String summary(){
+        return orderId + "|" + customer.label()+"|$"+total;
+    }
+}
+
+
+public class OrderCompositionDemo {
+    public static void main(String[] args){
+        Customer customer = new Customer("C101","Amy");
+        Order order = new Order("09001",customer,2500);
+
+        System.out.println(order.summary());
+    }
+}
