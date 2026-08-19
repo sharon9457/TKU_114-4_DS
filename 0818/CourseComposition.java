40c63b481e2f29cd7849bcc188c3345e4d3bd465
sharon9457 <sharon.cho.tw@gmail.com>
2026-08-19 16:02:33 +0800

    更改

---
+++ 0818/CourseComposition.java
@@ -0,0 +1,9 @@
+final class Instructor{
+    private final int id;
+    private final String name;
+
+
+}
+public class CourseComposition {
+    
+}
--- 0818/EquipmentInventory.java
+++ 0818/EquipmentInventory.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Constructor;
 
 class Equipment {
     private String id;
