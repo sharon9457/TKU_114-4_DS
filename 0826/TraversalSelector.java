68e684285142098fd48f7784f23d2f95703ef5bd
sharon9457 <sharon.cho.tw@gmail.com>
2026-08-29 13:24:54 +0800

    Update TraversalSelector.java

--- 0826/TraversalSelector.java
+++ 0826/TraversalSelector.java
@@ -1,98 +1,76 @@
+import java.beans.Expression;
 import java.util.ArrayList;
 import java.util.List;
 
 class ReviewNode{
-    int value;
+    String value;
     ReviewNode left;
     ReviewNode right;
 
-    ReviewNode(int value){
+    ReviewNode(String value){
         this.value = value;
     }
 }
 class  ReviewBst{
-    private ReviewNode root;
-    boolean add(int value){
-        if(root == null){
-            root = new ReviewNode(value);
-            return true;
-        }
-        ReviewNode current = root;
-        while(true){
-            if(value == current.value)return false;
-            if(value < current.value){
-                if(current.left == null){
-                    current.left = new ReviewNode(value);
-                        return true;
-                    }
-                    current = current.left;
-            }
-            current = current.left;
-        }else{
-            if(current.right == null){
-                current.right = new ReviewNode(value);
-                return true;
-            }
-            current = current.right;
-        }
-    }    
-}
-
-boolean contains(int traget){
-    ReviewNode current = root;
-    while(current != null){
-        if(target == current.value)return true;
-        current = target < current.value ? current.left:current.right;
+     ReviewNode root;
+    public void printPrefix(){
+        System.out.println("Preorder:");
+        preorder(root);
+        System.out.println();
+    }private void preorder(ReviewNode node){
+        if(node == null)return;
+        System.out.println(node.value);
+        preorder(node.left);
+        preorder(node.right);
     }
-    return false;
-}
-List<Integer>sarchPath(int target){
-    List<Integer>path = new ArrayList<>();
-    ReviewNode current = root;
-    while ((current != null)) {
-        path.add(current.value);
-        if(target == current.value)break;
-     current = target<current.value? current.left:current.right;   
+    private void inorder(ReviewNode node){
+        if(node == null)return;
+        boolean isOperator = (node.left != null || node.right != null);
+        if(isOperator){
+            System.out.print("(");
+        }inorder(node.left);
+        System.out.print(node.value);
+        inorder(node.right);
+        if(isOperator){
+            System.out.println(")");
         }
-        return path;
-}
-List<Integer>inorder(){
-    List<Integer>result = new ArrayList<>();
-    inorder(root,result);
-    return result;
-}
-private void inorder(ReviewNode node , List<Integer> result){
-    if(node == null)return;
-    inorder(node.left,result);
-    result.add(node.value);
-    inorder(node.right,result);
-}
-List<Integer> preorder(){
-    List<Integer> result = new ArrayList<>();
-    preorder(root,result);
-    return result;
-}
-private void preorder(ReviewNode node,List<Integer>result){
-    if(node == null) return;
-    result.add(node.value);
-    preorder(node.left,result);
-    preorder(node.right,result);
-}
-List<Integer>postorder(){
-    List<Integer> result = new ArrayList<>();
-    postorder(root,result);
-    return result;
-}
-private void postorder(ReviewNode node ,List<Integer>result){
-    if(node == null)return;
-    postorder(node.left, result);
-    postorder(node.right, result);
-    result.add(node.value);
-}
-int size(){
-    return size(root);
-}
-
-public class TraversalSelector {
+    }
+    public void printInfix(){
+        System.out.println("Inorder:");
+        inorder(root);
+        System.out.println();
+    }
     
+    
+
+public void printPostfix(){
+    System.out.println("Postorder:");
+    postorder(root);
+    System.out.println();
 }
+private void postorder(ReviewNode node){
+    if(node == null)return;
+    postorder(node.left);
+    postorder(node.right);
+    System.out.println(node.value +" ");
+}}
+public class TraversalSelector{
+    public static void main(String[] args) {
+        ReviewBst tree = new ReviewBst();
+
+        tree.root = new ReviewNode("*");
+        tree.root.left = new ReviewNode("+");
+        tree.root.right = new ReviewNode("-");
+
+        tree.root.left.left = new ReviewNode("A");
+        tree.root.left.right = new ReviewNode("A");
+
+         tree.root.right.left = new ReviewNode("C");
+        tree.root.right.right = new ReviewNode("D");
+
+        tree.printPrefix();
+        tree.printInfix();
+        tree.printPostfix();
+    }}
+
+
