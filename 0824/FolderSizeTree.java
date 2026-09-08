85be87ea4f0dc7a7fd9bc5a9eee4f7f68023f5cb
sharon9457 <sharon.cho.tw@gmail.com>
2026-09-08 20:09:31 +0800

    更改

--- 0824/FolderSizeTree.java
+++ 0824/FolderSizeTree.java
@@ -25,5 +25,32 @@ public class FolderSizeTree {
         
         int leftsize = calculate(node.left, stats);
         int rightsize = calculate(node.right, stats);
+
+        if(node.left == null && node.right == null){
+            stats.leftfolders.add(node.name);
+            }
+
+            int currentsize = node.ownsize+leftsize+rightsize;
+            if(currentsize>stats.maxsize){
+                stats.maxsize = currentsize;
+                stats.maxname = node.name;
+            }return currentsize;
+
+    }
+    public static void main(String[] args) {
+    FolderNode root1 = new FolderNode("root",1) ;
+    root1.left =new FolderNode("time",2);
+      root1.right = new FolderNode("temp",3);
+      root1.left.left = new FolderNode("work",4);
+      root1.left.right = new FolderNode("cul",5);
+      root1.right.left = new FolderNode("del",6);
+      root1.right.right = new FolderNode("caps",7);
+      Stats stats = new Stats();
+      int total = calculate(root1, stats);
+
+      System.out.println("total size:"+total);
+      System.out.println("Max:"+stats.maxname+"size:"+stats.maxsize);
+      System.out.println("folders"+stats.leftfolders);
+    
     }
 }
